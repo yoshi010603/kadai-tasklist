4bec704d7ffbbfc9c086ffa84b0e40ef19cd31f9
yoshiyuki.takahashi <yoshi010603@gmail.com>
2020-08-22 07:30:57 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+ 
   get 'signup', to: 'users#new'
   resources :tasks
   resources :users, only: [:index, :show, :new, :create]
